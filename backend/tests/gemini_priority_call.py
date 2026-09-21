bf2529602f2582b3141e95eebdbd6500888fc2c6
Jan Borwankar Visa <j.borwankar@student.maastrichtuniversity.nl>
2026-09-21 15:27:49 +0200

    gemini call 1 + input documentation
    The first gemini call which assigns expected time for tasks and prioritises them. Documentation on expected user inputs

---
+++ backend/tests/gemini_priority_call.py
@@ -0,0 +1,36 @@
+import os
+from google import genai
+
+client = genai.Client(api_key="AIzaSyAAJPUvnFdDx-wmCtAQ50rV29ubQKFex7M")
+
+myfile = client.files.upload(
+    file=r"C:\Users\root\Downloads\Timetable.csv"
+)
+
+tasks = "I need to edit 1hr of gameplay every day for my youtube channel, i dont really have any more stuff to do"
+
+food="30"
+
+sleep="8"
+
+cooking = "9"
+
+freetime = "1"
+
+commutetime = "15"
+
+response = client.models.generate_content(
+    model="gemini-3.5-flash",
+    contents=[
+        myfile,
+        tasks,
+        food,
+        sleep,
+        cooking,
+        freetime,
+        commutetime,
+        "Using the previous information, create a list of all tasks that need to be completed. Include level of priority, estimated time to complete (You can use the user inputted info when available), level of focus required (deep/casual), ideal time of day to do it, deadline (when relevant), for fixed events such as classes replace ideal time of day and expected time to complete by the start time and end time. The input is as follow: classes calendar file, description of tasks to do, time spent eating daily (mins), time spent sleeping daily(hrs), times person cooks every week(units, assume 25mins per cook, assign each cook to slots prioritising in this manner:weekday dinners>weekend dinners>weekend lunches>weekday lunches), preferred amount of daily free time(hrs, prioritise other tasks over this), time to commute from home to class"
+    ]
+)
+
+print(response.text)
